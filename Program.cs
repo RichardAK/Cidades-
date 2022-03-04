78fb3aa4ddc4bc6bc3ec85ea7f933300400333ba
Kauã <kau.lira@yahoo.com>
2022-03-04 17:58:37 -0300

    tarefa finalizada

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Cananéia.");
+Console.WriteLine("Iguape.");
+Console.WriteLine("Ilha Comprida.");
+Console.WriteLine("Itanhaém.");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Peruíbe.");
+Console.WriteLine("Praia Grande.");
